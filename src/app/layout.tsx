ebe68690c870d514994ff8a9a8befc98051aa5e9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-04-15 23:46:23 +0200

    responsive layout screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,13 @@ export default function RootLayout({
       </head>
       <body>
         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto flex justify-between">
-          <div className="">
+          <div className="px-2">
             <LeftBar />
           </div>
-          <div className="">
+          <div className="flex-1 lg:min-w[600px] border-x-[1px] border-gray-100">
             {children}
           </div>
-          <div className="">
+          <div className="hidden lg:flex ml-4 md:ml-8">
             <RightBar />
           </div>
         </div>
