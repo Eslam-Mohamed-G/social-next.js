6776e3d566e0102d38587475af509c59835666a9
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-05-17 19:49:08 +0200

    add image uploading

--- src/app/components/Share.tsx
+++ src/app/components/Share.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import Images from './Images';
 
 const Share = () => {
+    const [media, setMedia] = useState<File | null>(null);
+    const handleMediaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            setMedia(e.target.files[0])
+        };
+    }
     return (
-        <div className='flex gap-4 p-4'>
+        <form className='flex gap-4 p-4'>
             {/* avatar */}
             <div className='relative w-10 h-10 rounded-full overflow-hidden'>
-                <Images path='general/em.png' w={100} h={100} alt='logo'/>
+                <Images path='general/em.png' w={100} h={100} alt='logo' />
             </div>
 
             {/* inputs */}
@@ -17,17 +24,23 @@ const Share = () => {
                 <div className='flex justify-between items-center'>
                     {/* icons */}
                     <div className='flex gap-3 md:gap-4'>
-                        <Images path='icons/image.svg' alt='image' w={20} h={20} className='cursor-pointer'/>
-                        <Images path='icons/gif.svg' alt='gif' w={20} h={20} className='cursor-pointer'/>
-                        <Images path='icons/poll.svg' alt='poll' w={20} h={20} className='cursor-pointer'/>
-                        <Images path='icons/emoji.svg' alt='emoji' w={20} h={20} className='cursor-pointer'/>
-                        <Images path='icons/schedule.svg' alt='schedule' w={20} h={20} className='cursor-pointer'/>
-                        <Images path='icons/location.svg' alt='location' w={20} h={20} className='cursor-pointer'/>
+                        {/* upload images */}
+                        <div className=''>
+                            <input type="file" name="" id="file" className='hidden' onChange={handleMediaChange} />
+                            <label htmlFor="file">
+                                <Images path='icons/image.svg' alt='image' w={20} h={20} className='cursor-pointer' />
+                            </label>
+                        </div>
+                        <Images path='icons/gif.svg' alt='gif' w={20} h={20} className='cursor-pointer' />
+                        <Images path='icons/poll.svg' alt='poll' w={20} h={20} className='cursor-pointer' />
+                        <Images path='icons/emoji.svg' alt='emoji' w={20} h={20} className='cursor-pointer' />
+                        <Images path='icons/schedule.svg' alt='schedule' w={20} h={20} className='cursor-pointer' />
+                        <Images path='icons/location.svg' alt='location' w={20} h={20} className='cursor-pointer' />
                     </div>
                     <button className='bg-textGray rounded-full px-3 py-1 md:px-4 md:py-2 font-bold cursor-pointer flex items-center capitalize'>post</button>
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
 
