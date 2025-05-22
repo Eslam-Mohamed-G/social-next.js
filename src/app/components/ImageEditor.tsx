7c983340a1efc28b398a87924b914e5e346a8fea
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-05-22 18:00:26 +0200

    fun to handle change the type of image when edit it

--- src/app/components/ImageEditor.tsx
+++ src/app/components/ImageEditor.tsx
@@ -14,6 +14,9 @@ const ImageEditor = ({ onClose, previewURL, settings, setSettings }: {
     const handleChangeSensitive = (sensitive: boolean) => {
         setSettings(prev => ({ ...prev, sensitive }))
     };
+    const handleChangeType = (type: "original" | "wide" | "square") => {
+        setSettings(prev => ({ ...prev, type }))
+    };
     return (
         <div className='w-screen h-screen fixed top-0 left-0 bg-black/75 z-10 flex items-center justify-center'>
             <div className='bg-black rounded-xl p-12 flex flex-col gap-4'>
@@ -46,7 +49,40 @@ const ImageEditor = ({ onClose, previewURL, settings, setSettings }: {
 
                 {/* setting */}
                 <div className='flex items-center justify-between text-sm'>
-                    <div className='flex items-center gap-8'></div>
+                    <div className='flex items-center gap-8'>
+                        {/* original button */}
+                        <div className='flex items-center gap-2 cursor-pointer' onClick={()=>handleChangeType("original")}>
+                            <svg width={24} viewBox='0 0 24 24'>
+                                <path
+                                    className={settings.type === "original" ? "fill-iconBlue": "fill-[#c7c9ca]"}
+                                    d="M3 7.5C3 6.119 4.119 5 5.5 5h13C19.881 5 21 6.119 21 7.5v9c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 19 3 17.881 3 16.5v-9zM5.5 7c-.276 0-.5.224-.5.5v9c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-9c0-.276-.224-.5-.5-.5h-13z"
+                                />
+                            </svg>
+                            <span className='text-white'>original</span>
+                        </div>
+
+                        {/* wide button */}
+                        <div className='flex items-center gap-2 cursor-pointer' onClick={()=>handleChangeType("wide")}>
+                            <svg width={24} viewBox='0 0 24 24'>
+                                <path
+                                    className={settings.type === "wide" ? "fill-iconBlue": "fill-[#c7c9ca]"}
+                                    d="M3 9.5C3 8.119 4.119 7 5.5 7h13C19.881 7 21 8.119 21 9.5v5c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 17 3 15.881 3 14.5v-5zM5.5 9c-.276 0-.5.224-.5.5v5c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-5c0-.276-.224-.5-.5-.5h-13z"
+                                />
+                            </svg>
+                            <span className='text-white'>wide</span>
+                        </div>
+
+                        {/* square button */}
+                        <div className='flex items-center gap-2 cursor-pointer' onClick={()=>handleChangeType("square")}>
+                            <svg width={24} viewBox='0 0 24 24'>
+                                <path
+                                    className={settings.type === "square" ? "fill-iconBlue": "fill-white"}
+                                    d="M3 5.5C3 4.119 4.119 3 5.5 3h13C19.881 3 21 4.119 21 5.5v13c0 1.381-1.119 2.5-2.5 2.5h-13C4.119 21 3 19.881 3 18.5v-13zM5.5 5c-.276 0-.5.224-.5.5v13c0 .276.224.5.5.5h13c.276 0 .5-.224.5-.5v-13c0-.276-.224-.5-.5-.5h-13z"
+                                />
+                            </svg>
+                            <span className='text-white'>square</span>
+                        </div>
+                    </div>
                     <div className={`cursor-pointer py-1 px-4 rounded-full text-black ${settings.sensitive ? "bg-red-500" : "bg-white"}`}
                         onClick={() => handleChangeSensitive(!settings.sensitive)}
                     >
