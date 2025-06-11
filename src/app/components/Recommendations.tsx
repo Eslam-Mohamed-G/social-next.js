43ac9745e52538e22f7d27f657a0bf4c41779954
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-06-11 23:13:51 +0200

    add recommendations content

--- src/app/components/Recommendations.tsx
+++ src/app/components/Recommendations.tsx
@@ -1,9 +1,31 @@
+import Link from 'next/link';
 import React from 'react';
+import Images from './Images';
 
 const Recommendations = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col gap-4 p-4 rounded-2xl border-1 border-textGrayLight dark:border-borderGray'>
+            <p className='font-bold text-xl dark:text-textGrayLight'>Who to follow</p>
+            {/* user card */}
+            <div className=''>
+                <div className='flex gap-2'>
+                    <div className='relative rounded-full overflow-hidden w-12 h-12'>
+                        <Images path='general/me.jpeg' alt='avater' w={100} h={100} />
+                    </div>
+                    <div className='flex flex-1 items-center justify-between'>
+                        <div>
+                            <h1 className='text-md font-bold dark:text-textGrayLight'>john doe</h1>
+                            <span className='text-textGray text-sm'>@johonDoe</span>
+                        </div>
+                        <button className='bg-black dark:bg-white rounded-2xl capitalize font-bold text-textGrayLight dark:text-black h-fit px-4 py-1'>
+                            follow
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <Link href="/" className='text-iconBlue capitalize'>
+                show more
+            </Link>
         </div>
     );
 }
