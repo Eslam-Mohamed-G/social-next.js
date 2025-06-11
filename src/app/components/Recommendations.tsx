87c22f369dcd9245e8e77b054a9344274d29aef0
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-06-11 23:19:40 +0200

    add recommendations content

--- src/app/components/Recommendations.tsx
+++ src/app/components/Recommendations.tsx
@@ -4,11 +4,26 @@ import Images from './Images';
 
 const Recommendations = () => {
     return (
-        <div className='flex flex-col gap-4 p-4 rounded-2xl border-1 border-textGrayLight dark:border-borderGray'>
-            <p className='font-bold text-xl dark:text-textGrayLight'>Who to follow</p>
+        <div className='flex flex-col gap-2 rounded-2xl border-1 border-textGrayLight dark:border-borderGray'>
+            <p className='font-bold text-xl dark:text-textGrayLight px-4 pt-2'>Who to follow</p>
             {/* user card */}
             <div className=''>
-                <div className='flex gap-2'>
+                <div className='flex gap-2 px-4 py-2 hover:bg-textGrayLight dark:hover:bg-hoverBackGround'>
+                    <div className='relative rounded-full overflow-hidden w-12 h-12'>
+                        <Images path='general/me.jpeg' alt='avater' w={100} h={100} />
+                    </div>
+                    <div className='flex flex-1 items-center justify-between'>
+                        <div>
+                            <h1 className='text-md font-bold dark:text-textGrayLight'>john doe</h1>
+                            <span className='text-textGray text-sm'>@johonDoe</span>
+                        </div>
+                        <button className='bg-black dark:bg-white rounded-2xl capitalize font-bold text-textGrayLight dark:text-black h-fit px-4 py-1'>
+                            follow
+                        </button>
+                    </div>
+                </div>
+
+                <div className='flex gap-2 px-4 py-2 hover:bg-textGrayLight dark:hover:bg-hoverBackGround'>
                     <div className='relative rounded-full overflow-hidden w-12 h-12'>
                         <Images path='general/me.jpeg' alt='avater' w={100} h={100} />
                     </div>
@@ -23,7 +38,7 @@ const Recommendations = () => {
                     </div>
                 </div>
             </div>
-            <Link href="/" className='text-iconBlue capitalize'>
+            <Link href="/" className='text-iconBlue capitalize px-4 pb-2'>
                 show more
             </Link>
         </div>
