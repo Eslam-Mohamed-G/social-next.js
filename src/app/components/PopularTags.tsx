91cec92d77cf1062c68c896f6904907074225b83
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-06-12 16:11:13 +0200

    add PopularTags content

--- src/app/components/PopularTags.tsx
+++ src/app/components/PopularTags.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import Images from './Images';
+import Link from 'next/link';
+import PostInfo from './PostInfo';
 
 const PopularTags = () => {
     return (
-        <div className='p-4 rounded-2xl border-1 border-textGrayLight dark:border-borderGray flex flex-col gap-4'>
-            <h1 className='text-xl font-bold text-textGray'>what’s happening</h1>
+        <div className='rounded-2xl overflow-hidden border-1 border-textGrayLight dark:border-borderGray flex flex-col gap-4'>
+            <h1 className='text-xl font-bold text-black dark:text-textGrayLight pl-4 pt-4'>what’s happening</h1>
             {/* trends */}
-            <div className='flex gap-2'>
-                <div className='relative w-20 h-20 rounded-lg overflow-hidden'>
-                    <Images path='general/post.jpeg' alt='img' tr={true} w={120} h={120} />
-                </div>
-                <div className='flex-1'>
-                    <h2 className='font-bold text-textGrayLight'>everst</h2>
-                    <span className='text-sm text-textGray'>good night</span>
-                </div>
-            </div>
-
-            {/* topics */}
-            <div className=''>
+            <div className='px-4 hover:bg-textGrayLight dark:hover:bg-hoverBackGround cursor-pointer'>
                 <div className='flex items-center justify-between'>
-                    <span className='text-textGray font-semibold'>technology * trending</span>
+                    <span className='text-textGray capitalize text-sm'>trending in egypt</span>
+                    <PostInfo />
+                </div>
+                <div className='flex flex-col justify-center'>
+                    <span className='text-black dark:text-textGrayLight text-center font-semibold'>#trend name</span>
+                    <span className='text-textGray text-[12px]'>8k posts</span>
                 </div>
             </div>
+            <div className='px-4 hover:bg-textGrayLight dark:hover:bg-hoverBackGround cursor-pointer'>
+                <div className='flex items-center justify-between'>
+                    <span className='text-textGray capitalize text-sm'>trending in egypt</span>
+                    <PostInfo />
+                </div>
+                <div className='flex flex-col justify-center'>
+                    <span className='text-black dark:text-textGrayLight text-center font-semibold'>#trend name</span>
+                    <span className='text-textGray text-[12px]'>7.2k posts</span>
+                </div>
+            </div>
+            <Link href="/" className='text-iconBlue capitalize px-4 py-2 hover:bg-textGrayLight dark:hover:bg-hoverBackGround'>
+                show more
+            </Link>
         </div>
     );
 }
